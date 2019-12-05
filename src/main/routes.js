8590a10832bb2100a8f20cce36b341f14480f35f
Gburis <gabrielleiteolimpio@gmail.com>
2019-12-05 10:40:26 -0300

    Terminando principais rotas

--- src/main/routes.js
+++ src/main/routes.js
@@ -3,6 +3,9 @@ import {Route, Switch} from 'react-router-dom';
 
 import Home from './../pages/Home/Home';
 import Catalogo from './../pages/Catalogo/Catalogo'
+import Perfil from './../pages/Perfil/Perfil'
+import Produto from './../pages/Produto/Produto'
+import Login from './../pages/Login/Login'
 
 class Routes extends React.Component{
     render(){
@@ -12,6 +15,9 @@ class Routes extends React.Component{
                     <Route exact path='/' component={Home}/>
                     <Route exact path='/home' component={Home}/>
                     <Route exact path='/catalogo' component={Catalogo}/>
+                    <Route exact path='/perfil' component={Perfil}/>
+                    <Route exact path='/produto' component={Produto}/>
+                    <Route exact path='/login' component={Login}/>
                 </Switch>
             </main>
         );
---
+++ src/pages/Produto/Produto.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Produto extends React.Component{
+    render(){
+        return(
+            <h1>Produto</h1>
+        );
+    }
+}
+
+export default Produto;
